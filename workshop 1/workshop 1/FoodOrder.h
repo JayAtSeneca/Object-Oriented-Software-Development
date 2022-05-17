e82b35f62e17dea8878829c7540c31ccf6d190d5
JayAtSeneca <jpchaudhari@myseneca.ca>
2022-05-17 13:05:19 -0400

    Added workshop 1 empty files and gitgnore file

---
+++ workshop 1/workshop 1/FoodOrder.h
@@ -0,0 +1,11 @@
+#ifndef SDDS_FOOD_ORDER_H
+#define SDDS_FOOD_ORDER_H
+namespace sdds
+{
+	extern double g_taxrate;
+	extern double g_dailydiscount;
+	class FoodOrder {
+
+	};
+}
+#endif // !SDDS_FOOD_ORDER_H
