6f9765489a26a9c80601e48d2e3e2ab2ba5a87ac
JayAtSeneca <jpchaudhari@myseneca.ca>
2022-06-05 01:46:54 -0400

    w2 done

---
+++ workshop 2/workshop-2/ProteinDatabase.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <fstream>
+#include "ProteinDatabase.h"
+using namespace std;
+
+namespace sdds {
+
+	ProteinDatabase::ProteinDatabase() {}
+	ProteinDatabase::ProteinDatabase(const char* fileName) {
+		std::ifstream ifstr(fileName);
+		std::string temp;
+		if (ifstr) {
+			while (getline(ifstr, temp, '\n')) {
+				if (temp[0] == '>')
+					m_size++;
+			}
+
+			if (m_size > 0) {
+				ifstr.clear();
+				ifstr.seekg(0);
+				m_proteinArr = new std::string[m_size];
+
+				int index = -1;
+				while (getline(ifstr, temp, '\n')) {
+					if (temp[0] == '>') {
+						index++;
+					}
+					else {
+						m_proteinArr[index] += temp;
+					}
+				}
+			}
+		}
+	}
+
+	unsigned int ProteinDatabase::size()const {
+		return m_size;
+	}
+
+	std::string ProteinDatabase::operator[](unsigned int index)const {
+		std::string str = (index < m_size) ? m_proteinArr[index] : "";
+		return str;
+	}
+
+	void ProteinDatabase::safeEmpty() {
+		delete[] m_proteinArr;
+		m_proteinArr = nullptr;
+		m_size = 0;
+	}
+
+	ProteinDatabase::ProteinDatabase(const ProteinDatabase& src) {
+		*this = src;
+	}
+
+	ProteinDatabase& ProteinDatabase::operator=(const ProteinDatabase& src) {
+		if (this != &src) {
+			safeEmpty();
+			m_size = src.m_size;
+			m_proteinArr = new std::string[m_size];
+			for (unsigned int i = 0u; i < m_size; i++) {
+				m_proteinArr[i] = src.m_proteinArr[i];
+			}
+		}
+		return *this;
+	}
+
+	ProteinDatabase::ProteinDatabase(ProteinDatabase&& src) noexcept{
+		*this = std::move(src);
+	}
+
+	ProteinDatabase& ProteinDatabase::operator=(ProteinDatabase&& src) noexcept {
+		if (this != &src) {
+			safeEmpty();
+			m_size = src.m_size;
+			m_proteinArr = src.m_proteinArr;
+			src.m_proteinArr = nullptr;
+			src.m_size = 0;
+		}
+		return *this;
+	}
+
+	ProteinDatabase:: ~ProteinDatabase() {
+		delete[] m_proteinArr;
+	}
+}
---
+++ workshop 2/workshop-2/TimedTask.h
@@ -0,0 +1,36 @@
+#ifndef SDDS_TIMEDTASK_H
+#define SDDS_TIMEDTASK_H
+#include <iostream>
+#include <chrono>
+#include <string>
+
+namespace sdds {
+
+	const int MAX_OBJECTS = 10;
+	struct Task {
+		std::string m_task;
+		std::string m_timeUnit;
+		std::chrono::steady_clock::duration m_duration;
+	};
+	class TimedTask {
+
+		unsigned int m_recordNum = 0u;
+		std::chrono::steady_clock::time_point m_startTime;
+		std::chrono::steady_clock::time_point m_endTime;
+
+		struct Task taskList[MAX_OBJECTS]{};
+
+	public:
+		TimedTask();
+		virtual ~TimedTask();
+		void startClock();
+		void stopClock();
+		void addTask(const char* taskName);
+		friend std::ostream& operator<<(std::ostream& os, const TimedTask& src);
+
+	};
+
+
+}
+#endif // !SDDS_TIMEDTASK_H
+
