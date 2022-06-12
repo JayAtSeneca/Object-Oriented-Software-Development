1d5d1a547ee21f1c0542de50ffc9bdf3b2847a84
JayAtSeneca <jpchaudhari@myseneca.ca>
2022-06-12 14:21:05 -0400

    code examples done

---
+++ week4Notes/week4Notes/PlaydohPackAgg.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+class Playdoh;
+const int packsize = 6;
+class PlaydohPackAgg {
+	std::string name{ "Unknown" };
+	int dohCount{ 0 };
+	const Playdoh* playdohs[packsize]{};
+public:
+	PlaydohPackAgg();
+	PlaydohPackAgg(const char* nm);
+	~PlaydohPackAgg();
+	std::ostream& display(std::ostream& os)const;
+	PlaydohPackAgg& operator+=(const Playdoh& src);
+	PlaydohPackAgg& operator-=(const Playdoh& src);
+
+};
+std::ostream& operator<<(std::ostream& os, const PlaydohPackAgg& p);
