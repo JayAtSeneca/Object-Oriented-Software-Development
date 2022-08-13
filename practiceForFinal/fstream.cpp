d7143bacf8cc56ad963f40b611a7f09ba4b93e3e
JayAtSeneca <jpchaudhari@myseneca.ca>
2022-08-13 15:25:48 -0400

    thread class function object

--- practiceForFinal/fstream.cpp
+++ practiceForFinal/fstream.cpp
@@ -1,5 +1,5 @@
-// Thread Class - Thread Identifiers
- // thread_id.cpp
+// Thread Class - Function Object
+ // thread_id_fo.cpp
 
  #include <iostream>
  #include <thread>
@@ -7,18 +7,22 @@
 
  const int NT = 10;
 
- void task() {
-     std::cout << "Thread id = " <<
+ class Task {
+   public:
+     Task(){}
+     void operator()(int i) {
+     std::cout << i << " Thread id = " <<
       std::this_thread::get_id() << std::endl; 
- }
+     }
+ };
 
  int main() {
-     // create a vector of threads
+     // create a vector of not-joinable threads
      std::vector<std::thread> threads;
 
      // launch execution of each thread
      for (int i = 0; i < NT; i++)
-         threads.push_back(std::thread(task)); 
+         threads.push_back(std::thread(Task(), i)); 
 
      // synchronize their execution here
      for (auto& thread : threads)
