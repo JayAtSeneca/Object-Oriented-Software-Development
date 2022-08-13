89fad215c0ce39b1302923f07e3a27b027b1e186
JayAtSeneca <jpchaudhari@myseneca.ca>
2022-08-13 16:20:23 -0400

    Future Class Template - Explicit Asynchronous Launch

--- practiceForFinal/fstream.cpp
+++ practiceForFinal/fstream.cpp
@@ -1,14 +1,36 @@
-// Asynchronous Launch
- // async.cpp
+// Future Class Template - Explicit Asynchronous Launch
+ // future_async.cpp
 
  #include <iostream>
- #include <thread>
  #include <future>
 
- double task(double x) { return x * 2; }
+ double get() { return 12.34; }
 
  int main() {
-     std::future<double> f = std::async(task, 10); 
-     double r = f.get();
-     std::cout << "Result = " << r << std::endl; 
+     std::future<double> f; // default ctor
+     std::future<double> g = std::async(get); // move-ctor 
+
+     std::cout << "After Construction" << std::endl;
+     std::cout << (f.valid() ? "f is valid" :
+      "f is not valid") << std::endl; 
+     std::cout << (g.valid() ? "g is valid" :
+      "g is not valid") << std::endl; 
+
+     f = std::move(g); // move-assignment
+
+     std::cout << "After Assignment" << std::endl;
+     std::cout << (f.valid() ? "f is valid" :
+      "f is not valid") << std::endl; 
+     std::cout << (g.valid() ? "g is valid" :
+      "g is not valid") << std::endl; 
+
+     double a = f.get(); // retrieve shared value
+
+     std::cout << "After Retrieval" << std::endl;
+     std::cout << (f.valid() ? "f is valid" :
+      "f is not valid") << std::endl; 
+     std::cout << (g.valid() ? "g is valid" :
+      "g is not valid") << std::endl; 
+
+     std::cout << "Return Value = " << a << std::endl;
  }
