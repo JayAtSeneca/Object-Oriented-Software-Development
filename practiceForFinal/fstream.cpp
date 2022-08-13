0e8bba56f122e9ad09c4d4b84eff632a63926928
JayAtSeneca <jpchaudhari@myseneca.ca>
2022-08-13 15:26:57 -0400

    thread identifiers with lambda expressions

--- practiceForFinal/fstream.cpp
+++ practiceForFinal/fstream.cpp
@@ -1,5 +1,5 @@
-// Thread Class - Function Object
- // thread_id_fo.cpp
+// Thread Class - Lambda Expression
+ // thread_id_lambda.cpp
 
  #include <iostream>
  #include <thread>
@@ -7,22 +7,16 @@
 
  const int NT = 10;
 
- class Task {
-   public:
-     Task(){}
-     void operator()(int i) {
-     std::cout << i << " Thread id = " <<
-      std::this_thread::get_id() << std::endl; 
-     }
- };
-
  int main() {
      // create a vector of not-joinable threads
      std::vector<std::thread> threads;
 
-     // launch execution of each thread
+     // launch the execution of each thread
      for (int i = 0; i < NT; i++)
-         threads.push_back(std::thread(Task(), i)); 
+         threads.push_back(std::thread([=]() {
+             std::cout << i << " Thread id = " <<
+              std::this_thread::get_id() << std::endl; 
+         })); 
 
      // synchronize their execution here
      for (auto& thread : threads)
