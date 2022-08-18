0b9b4c1ae8756ea9cecc62f5c473076b99e64ea2
JayAtSeneca <jpchaudhari@myseneca.ca>
2022-08-17 21:54:03 -0400

    Container Adapters - Stacks

--- practiceForFinal/string.cpp
+++ practiceForFinal/string.cpp
@@ -1,22 +1,19 @@
-// Sequential Containers - Double-Ended Queues
- // deque.cpp
+// Container Adapters - Stacks
+ // ca_stack.cpp
 
- #include <deque>  // for deque template
+ #include <stack>
  #include <iostream>
 
  int main() {
-     std::deque<double> prices(3, 10.50), costs; 
+     std::stack<double> prices; // initially empty 
 
-     prices.back() = 32.43; // reset last
-     prices.pop_front();    // remove first
-     for(int i = 0; i < prices.size(); i++) 
-         std::cout << prices[i] << "  ";
-     std::cout << std::endl;
-     costs = prices;
-     costs.push_front(5.64);  // add 5.64
-     costs.push_front(20.31); // add 20.31
-     costs.at(1) += 10.0;     // add 10.0
-     for(int i = 0; i < costs.size(); i++)
-         std::cout << costs[i] << "  ";
+     prices.push(10.43); // add 10.43
+     prices.push(20.54); // add 20.54
+     prices.push(32.43); // add 32.43
+     prices.top() = 5.41;
+     while(!prices.empty()) {
+         std::cout << prices.top() << "  ";
+         prices.pop();
+     }
      std::cout << std::endl;
  }
