1fcb77bb14f19fb2ac8dbc333d0b3b5935e2d23c
JayAtSeneca <jpchaudhari@myseneca.ca>
2022-08-17 21:39:41 -0400

    Sequential Containers - Double-Ended Queues

--- practiceForFinal/string.cpp
+++ practiceForFinal/string.cpp
@@ -1,23 +1,22 @@
-// Sequential Containers - Vectors
- // vector.cpp
+// Sequential Containers - Double-Ended Queues
+ // deque.cpp
 
- #include <vector>
+ #include <deque>  // for deque template
  #include <iostream>
 
  int main() {
-     std::vector<double> prices; // initially empty
+     std::deque<double> prices(3, 10.50), costs; 
 
-     if(prices.empty())       // is prices empty?
-         std::cout << "prices is empty" << std::endl; 
-     prices.push_back(10.43); // add 10.43
-     prices.push_back(20.54); // add 20.54
-     prices.push_back(32.43); // add 32.43
-     for(int i = 0; i < prices.size(); i++)
+     prices.back() = 32.43; // reset last
+     prices.pop_front();    // remove first
+     for(int i = 0; i < prices.size(); i++) 
          std::cout << prices[i] << "  ";
      std::cout << std::endl;
-     prices.front() = 54.11; // change 1st element 
-     prices.pop_back();      // remove last element 
-     for(int i = 0; i < prices.size(); i++)
-         std::cout << prices[i] << "  ";
+     costs = prices;
+     costs.push_front(5.64);  // add 5.64
+     costs.push_front(20.31); // add 20.31
+     costs.at(1) += 10.0;     // add 10.0
+     for(int i = 0; i < costs.size(); i++)
+         std::cout << costs[i] << "  ";
      std::cout << std::endl;
  }
