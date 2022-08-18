c440d7f6b885a2c0d228a1478f0114c2a4ca006a
JayAtSeneca <jpchaudhari@myseneca.ca>
2022-08-17 22:16:46 -0400

    Iterators - Insertion and Removal

--- practiceForFinal/string.cpp
+++ practiceForFinal/string.cpp
@@ -1,17 +1,18 @@
-// Iterators - Vectors
- // iterator.cpp
+// Iterators - Insertion and Removal
+ // iterator_list.cpp
 
- #include <vector>
+ #include <list>
  #include <iostream>
 
  int main() {
-     std::vector<double> prices;  // initially empty
-     std::vector<double>::iterator i;
+     std::list<double> prices;  // initially empty
 
-     prices.push_back(10.43); // add 10.43
-     prices.push_back(20.54); // add 20.54
-     prices.push_back(32.43); // add 32.43
-     for(i = prices.begin(); i != prices.end(); i++) 
+     prices.push_back(10.43);   // add 10.43
+     prices.push_back(20.54);   // add 20.54
+     prices.push_back(32.43);   // add 32.43
+     prices.insert(--prices.end(), 12.52);
+     prices.erase(++prices.begin());
+     for(auto i = prices.begin(); i != prices.end(); i++) 
          std::cout << *i << "  ";
      std::cout << std::endl;
  }
